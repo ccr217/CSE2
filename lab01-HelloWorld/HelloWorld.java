27c775c09a9ec86d5f998d9e99539d345be55dcc
ccr217 <ccr217@lehigh.edu>
2014-08-29 14:51:18 +0000

    updated HelloWolrd

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+
+//Caroline Ruffing 
+//Lab 01
+//Hello World Java Program
+//
+//  define a class
+public class HelloWorld{
+    
+//  add main method
+public static void main(String[ ] args) {
+    
+//  print the statement with the box around it
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello World 2014!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    //test/testtest
+}
+}
