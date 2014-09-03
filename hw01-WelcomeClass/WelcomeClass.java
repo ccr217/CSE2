2bee42d04a3f81b8536b87b8c41a39d527290d19
ccr217 <ccr217@lehigh.edu>
2014-09-03 18:46:53 +0000

    updated WelcomeClass

---
+++ hw01-WelcomeClass/WelcomeClass.java
@@ -0,0 +1,26 @@
+
+//Caroline Ruffing 
+//hw01
+//hw01 Java Program
+//
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+public static void main(String[ ] args) {
+    
+//  print the statement with the box around it
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-C--C--R--2--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("I spent the summer teaching tennis at a camp in Virginia.");
+    System.out.println("test");
+    
+    //test/testtest
+}
+}
